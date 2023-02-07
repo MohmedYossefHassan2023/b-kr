305f81da16f2f94a984e154c93352f1e7ff6fe30
MohmedYossefHassan2023 <pcfaster5@gmail,com>
2023-02-07 13:51:10 +0200

    b@kr

---
+++ my-react-app/src/components/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+
+function App() {
+  return (
+    <div>
+      <h1>WELLCOM  b@kr</h1>
+      <p>We are a world-leading genomics research 
+        institute. Our work helps improve human health and understand life on Earth.</p>
+      <ul class="list-group">
+  <li class="list-group-item">mohamed</li>
+  <li class="list-group-item">yossef</li>
+  <li class="list-group-item"> hamza</li>
+  
+</ul>
+    </div>
+  );
+}
+
+export default App;
